2804b3777cf1d64270f2c495e5b68004203fe778
ChanHyeokLee20180515 <94631666+ChanHyeokLee20180515@users.noreply.github.com>
2021-12-07 20:01:37 +0900

    Update shell.c
    Make a shell to accept an arbitrary message.

--- shell.c
+++ shell.c
@@ -4,16 +4,28 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 int main() {
-  int i, pid;
-  char *token, command[2000], *arguments[10];
+  int i = 0, pid, count = 0;
+  char *token, command[2000];
   fgets(command, sizeof(command), stdin);
   command[strlen(command)-1] = 0;
+  while(command[i] != '\0'){
+      if(isspace(command[i])){
+          count++;
+      }
+      i++;
+  }
+  count++;
+  char *arguments[count];
+  printf("%d\n",count);
+
+
   token = strtok(command, " ");
   if (token == NULL) {exit(-1);}
   arguments[0] = token;
-  for (i = 1; i<10; i++) {
+  for (i = 1; i<count; i++) {
     token = strtok(NULL, " ");
     if (token == NULL)
       break;
